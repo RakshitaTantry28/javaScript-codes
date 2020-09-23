f3adc166a8164fb4d88bb2174ce25b9b2bacd570
RakshitaTantry28 <55851040+RakshitaTantry28@users.noreply.github.com>
2020-09-23 21:25:20 +0530

    Add files via upload

---
+++ Cash Register.js
@@ -0,0 +1,101 @@
+/*
+Design a cash register drawer function checkCashRegister() that accepts purchase price as the first argument (price), payment as the second argument (cash), and cash-in-drawer (cid) as the third argument.
+
+cid is a 2D array listing available currency.
+
+The checkCashRegister() function should always return an object with a status key and a change key.
+
+Return {status: "INSUFFICIENT_FUNDS", change: []} if cash-in-drawer is less than the change due, or if you cannot return the exact change.
+
+Return {status: "CLOSED", change: [...]} with cash-in-drawer as the value for the key change if it is equal to the change due.
+
+Otherwise, return {status: "OPEN", change: [...]}, with the change due in coins and bills, sorted in highest to lowest order, as the value of the change key.
+
+Currency Unit	Amount
+Penny	$0.01 (PENNY)
+Nickel	$0.05 (NICKEL)
+Dime	$0.1 (DIME)
+Quarter	$0.25 (QUARTER)
+Dollar	$1 (ONE)
+Five Dollars	$5 (FIVE)
+Ten Dollars	$10 (TEN)
+Twenty Dollars	$20 (TWENTY)
+One-hundred Dollars	$100 (ONE HUNDRED)
+
+*/
+
+//solution
+
+var denomination = [
+  { name: "ONE HUNDRED", val: 100.0 },
+  { name: "TWENTY", val: 20.0 },
+  { name: "TEN", val: 10.0 },
+  { name: "FIVE", val: 5.0 },
+  { name: "ONE", val: 1.0 },
+  { name: "QUARTER", val: 0.25 },
+  { name: "DIME", val: 0.1 },
+  { name: "NICKEL", val: 0.05 },
+  { name: "PENNY", val: 0.01 }
+];
+
+function checkCashRegister(price, cash, cid) {
+  var change = [];
+  var change_due = cash-price;
+  
+var register = cid.reduce(
+    function(acc, curr) {
+      acc.total += curr[1];
+      acc[curr[0]] = curr[1];
+      return acc;
+    },
+    { total: 0 }
+  );
+   console.log(register)
+
+  
+
+  if(register.total < change_due){
+    console.log({status: "INSUFFICIENT_FUNDS", change: []})
+    return {status: "INSUFFICIENT_FUNDS", change: []}
+  }
+
+  
+
+  else if(register.total == change_due){
+    console.log({status: "CLOSED", change: cid})
+    return {status: "CLOSED", change: cid}
+  }
+
+  else{
+    // Loop through the denomination array
+  var change_arr = denomination.reduce(function(acc, curr) {
+    var value = 0;
+    
+    while (register[curr.name] > 0 && change_due >= curr.val) {
+      change_due -= curr.val;
+      register[curr.name] -= curr.val;
+      value += curr.val;
+
+
+      change_due = Math.round(change_due * 100) / 100;
+    }
+    
+    if (value > 0) {
+      acc.push([curr.name, value]);
+    }
+    return acc; // Return the current change_arr
+  }, []); // Initial value of empty array for reduce
+
+  // If there are no elements in change_arr or we have leftover change, return
+  // the string "Insufficient Funds"
+  if (change_arr.length < 1 || change_due > 0) {
+    return {status: "INSUFFICIENT_FUNDS", change: []};
+  }
+ 
+ console.log({status: "OPEN", change: change_arr})
+  return {status: "OPEN", change: change_arr};
+  }
+}
+
+console.log(checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
+
